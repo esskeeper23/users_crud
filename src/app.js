6d5e774876628900d26e018a2e2ecb2dbe9fd3b8
esskeeper23 <rene2.martinez@outlook.com>
2022-10-30 20:03:16 -0700

    first commit

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+const port = 9000
+
+app.use(express.json)
+
+app.get('/',(req, res) => {
+    res.status(200).json({message: 'OK!'})
+})
+
+app.listen(port, () =>{
+    console.log(`Server started at port ${port}`)
+})
